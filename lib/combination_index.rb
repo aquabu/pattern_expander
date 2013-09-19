66ab43a78171361f2ca219b6fbaef98c6095970a
Noah Thorp <pair+noah@sharespost.com>
2013-09-19 08:57:09 -0700

    Refactor combination_index_spec for clarity

--- lib/combination_index.rb
+++ lib/combination_index.rb
@@ -6,24 +6,32 @@ class CombinationIndex
   end
 
   def [](i)
-    element_list_indexes = _multi_array_indexes(i,element_lists.map(&:size))
     combination = ""
-    element_lists.each_with_index do |element_list, index|
-      combination += element_list[element_list_indexes[index]]
+    element_list_indexes = _multi_array_indexes(i)
+    element_lists.each do |element_list|
+      element_list_index = element_list_indexes.shift
+      combination += element_list[element_list_index]
     end
+
     combination
   end
 
   # converts a single index to several indexes
   # ie converts from base ten (index) to a mixed base value
   # with each place (element array) being a new base (size of the element array)
-  def _multi_array_indexes(i, sizes)
+  def _multi_array_indexes(i)
     array_indexes = []
-    sizes.reverse.reduce(i) do |carried_index, base|
-      carried_index, remainder = carried_index.divmod(base)
+
+    _element_list_sizes.reverse.reduce(i) do |i_memo, list_size|
+      i_memo, remainder = i_memo.divmod(list_size)
       array_indexes << remainder
-      carried_index
+      i_memo
     end
+
     array_indexes.reverse
+  end
+
+  def _element_list_sizes
+    element_lists.map(&:size)
   end
 end
--- spec/lib/combination_index_spec.rb
+++ spec/lib/combination_index_spec.rb
@@ -16,28 +16,35 @@ describe CombinationIndex do
 
   describe '#_multi_array_indexes' do
     it 'should take an index and an array of index sizes and return an array of indexes' do
-      combination_index._multi_array_indexes(0,[2,2,2]).should == [0,0,0]
-      combination_index._multi_array_indexes(1,[2,2,2]).should == [0,0,1]
-      combination_index._multi_array_indexes(2,[2,2,2]).should == [0,1,0]
-      combination_index._multi_array_indexes(3,[2,2,2]).should == [0,1,1]
-      combination_index._multi_array_indexes(4,[2,2,2]).should == [1,0,0]
+      combination_index = CombinationIndex.new([[:a,:b],[:a,:b],[:a,:b]])
+      combination_index._multi_array_indexes(0).should == [0,0,0]
+      combination_index._multi_array_indexes(1).should == [0,0,1]
+      combination_index._multi_array_indexes(2).should == [0,1,0]
+      combination_index._multi_array_indexes(3).should == [0,1,1]
+      combination_index._multi_array_indexes(4).should == [1,0,0]
     end
 
     it 'can handle a collection of index sizes of variable length' do
-      combination_index._multi_array_indexes(8,[2,2,2,2]).should == [1,0,0,0]
-      combination_index._multi_array_indexes(15,[2,2,2,2]).should == [1,1,1,1]
+      combination_index = CombinationIndex.new([[:a,:b],[:a,:b],[:a,:b],[:a,:b]])
+      combination_index._multi_array_indexes(8).should == [1,0,0,0]
+      combination_index._multi_array_indexes(15).should == [1,1,1,1]
     end
 
     it 'can handle a collection of indexes with different sizes' do
-      combination_index._multi_array_indexes(0,[5,3]).should == [0,0]
-      combination_index._multi_array_indexes(2,[5,3]).should == [0,2]
-      combination_index._multi_array_indexes(3,[5,3]).should == [1,0]
-      combination_index._multi_array_indexes(14,[5,3]).should == [4,2]
+      combination_index = CombinationIndex.new([[:a,:b,:c,:d,:e],[:a,:b,:c]])
+      combination_index._multi_array_indexes(0).should == [0,0]
+      combination_index._multi_array_indexes(2).should == [0,2]
+      combination_index._multi_array_indexes(3).should == [1,0]
+      combination_index._multi_array_indexes(14).should == [4,2]
     end
 
     it 'can handle many indexes of varying sizes' do
-      combination_index._multi_array_indexes(0,[6,5,4,3,2]).should == [0,0,0,0,0]
-      combination_index._multi_array_indexes(719,[6,5,4,3,2]).should == [5,4,3,2,1]
+      a = [:a]
+      combination_index = CombinationIndex.new(
+        [a * 6, a * 5, a * 4, a * 3, a * 2]
+      )
+      combination_index._multi_array_indexes(0).should == [0,0,0,0,0]
+      combination_index._multi_array_indexes(719).should == [5,4,3,2,1]
     end
   end
 end
